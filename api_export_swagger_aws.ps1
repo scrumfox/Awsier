28d314cb05639101f75b6c175538efd343cc9e6d
scrumfox <13447916+scrumfox@users.noreply.github.com>
2022-06-13 13:58:03 +0530

    Update api_export_swagger_aws.ps1

--- api_export_swagger_aws.ps1
+++ api_export_swagger_aws.ps1
@@ -5,14 +5,20 @@
 ##
 ## Prerequisities: Need to set up the region and credentials using aws configure.
 #-------------------------------------------------------------
-
+Write-Host "				                                           "
 Write-Host "				__________       _____________             "
 Write-Host "				___    |_ |     / /_  ___/__(_)____________"
 Write-Host "				__  /| |_ | /| / /_____ \__  /_  _ \_  ___/"
 Write-Host "				_  ___ |_ |/ |/ / ____/ /_  / /  __/  /    "
 Write-Host "				/_/  |_|___/|__/  /____/ /_/  \___//_/     "
-Write-Host ""                                       
-										 
+Write-Host ""    
+Write-Host ""
+Write-Host ""    
+Write-Host " Awsier by SeKurly Team."
+Write-Host " Cloud security auditing enabling tool for AWS. Generates the Open api documentation for all the apis in the preset AWS region, which help to conduct the API Security analysis. The json documents will be named with the corresponding gateway identifier."                               
+Write-Host ""
+Write-Host "INFO: Need to set up the region and credentials using aws configure"
+Write-Host "INFO: Collecting the gateway ids"            										 
 $gatwayids = aws apigateway get-rest-apis --query items[*].id --output text
 
 if(-not ([string]::IsNullOrEmpty($gatwayids))){
@@ -32,4 +38,8 @@ if(-not ([string]::IsNullOrEmpty($gatwayids))){
 			}
 		}
 	}
+}
+else
+{
+	Write-Host "INFO: No gateway ids present."            										 
 }
